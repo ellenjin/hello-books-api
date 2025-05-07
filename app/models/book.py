9bf983b949b42aaa948ac675a8585e48d50c9380
Solhee <ellen.solhee.jin@gmail.com>
2025-05-07 09:55:48 -0700

    Created Author model

--- app/models/book.py
+++ app/models/book.py
@@ -1,16 +1,22 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
 from typing import Optional
 from ..db import db
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+  from .author import Author
 
 class Book(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
     description: Mapped[str]
+    author_id: Mapped[Optional[int]] = mapped_column(ForeignKey("author.id"))
+    author: Mapped[Optional["Author"]] = relationship(back_populates="books")
     # description: Mapped[Optional[str]] = mapped_column(nullable=True) # change this back to Mapped[str] later
 
     @classmethod
     def from_dict(cls, book_data):
-        new_book = Book(title=book_data["title"],
+        new_book = cls(title=book_data["title"],
                         description=book_data["description"])
         return new_book
 
--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,17 +1,15 @@
 from flask import Blueprint, abort, make_response, request, Response
 from app.models.book import Book
 from ..db import db
+from .route_utilities import validate_model
 
-books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
+bp = Blueprint("bp", __name__, url_prefix="/books")
 
-@books_bp.post("")
+@bp.post("")
 def create_book():
     request_body = request.get_json()
 
     try: 
-        # title = request_body["title"]
-        # description = request_body["description"]
-        # new_book = Book(title=title, description=description)
         new_book = Book.from_dict(request_body)
     
         
@@ -24,7 +22,7 @@ def create_book():
 
     return new_book.to_dict(), 201
 
-@books_bp.get("")
+@bp.get("")
 def get_all_books():
     query = db.select(Book)
 
@@ -43,43 +41,29 @@ def get_all_books():
         books_response.append(book.to_dict())
     return books_response
 
-@books_bp.get("/<book_id>")
+@bp.get("/<book_id>")
 def get_one_book(book_id):
-    book = validate_book(book_id)
+    book = validate_model(Book, book_id)
 
     return book.to_dict()
 
-def validate_book(book_id):
-    try:
-        book_id = int(book_id)
-    except:
-        response = {"message": f"Book {book_id} invalid"}
-        abort(make_response(response, 400))
-    
-    query = db.select(Book).where(Book.id == book_id)
-    book = db.session.scalar(query)
-
-    if not book:
-        response = {"message": f"Book {book_id} not found"}
-        abort(make_response(response, 404))
-    
-    return book
-
-@books_bp.put("/<book_id>")
+@bp.put("/<book_id>")
 def update_book(book_id):
-    book = validate_book(book_id)
+    book = validate_model(Book, book_id)
     request_body = request.get_json()
 
     book.title = request_body["title"]
     book.description = request_body["description"]
     db.session.commit()
 
-    return Response(status=204, mimetype="application/json")
+    # return Response(status=204, mimetype="application/json") # causing errors with new test?
+    return {"message":f"Book #{book.id} successfully updated"}, 200
 
-@books_bp.delete("/<book_id>")
+@bp.delete("/<book_id>")
 def delete_book(book_id):
-    book = validate_book(book_id)
+    book = validate_model(Book, book_id)
     db.session.delete(book)
     db.session.commit()
 
-    return Response(status=204, mimetype="application/json")
+    # return Response(status=204, mimetype="application/json")
+    return make_response({"message":f"Book #{book.id} successfully deleted"}, 200)
